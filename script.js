7e2ff62fc68f7758c7a822471474cb2d5ac2debd
Nisha Bukke <sononenisha827@gmail.com>
2025-10-13 15:25:56 +0530

    dastur primary secondary changes

--- script.js
+++ script.js
@@ -23,3 +23,31 @@ function displaySection(evt, id) {
    evt.currentTarget.className += " d-chart-show";
 
 }
+
+// sticky header
+
+$(window).on("scroll touchmove", function() {
+
+  if ($(document).scrollTop() > $("#sticky-wrapper").position().top) {
+    $('.sticky-area').css('width', '100%');
+    $('.sticky-area').css('max-width', '1600px');
+    $('.sticky-area').css('margin', '0 auto');
+    $('.sticky-area').css('position', 'fixed');
+    $('.sticky-area').css('top', '0px');
+    $('.sticky-area').css('background', '#e9f2ff');
+    $('.sticky-area ul.nav .nav-li > a').css('color', '#085291');
+    $('.school-name').css('color', '#085291');
+    $('.sticky-area ul li a').css('font-weight', '700');
+    $('.sticky-area').css('z-index', '111');
+    $('.sticky-area').css('box-shadow', '0 3px 5px -2px #ebe8e8');
+    $('#sticky-wrapper').addClass('is-sticky');
+
+  }
+  else{
+  $('.sticky-area').removeAttr('style');
+  $('.sticky-area ul.nav .nav-li > a').removeAttr('style');
+  $('.school-name').removeAttr('style');
+
+  }
+  
+});
