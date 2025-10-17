79900ebac17e45b4873b2d3fbb21a836dd809f68
Nisha Bukke <sononenisha827@gmail.com>
2025-10-17 12:53:46 +0530

    dastur primary responsive development

--- script.js
+++ script.js
@@ -24,6 +24,44 @@ function displaySection(evt, id) {
 
 }
 
+
+// mobile menu
+
+$( document ).ready(function() {
+
+  $( ".cross" ).hide();
+  $( ".menu" ).hide();
+  $( ".canva_expander" ).click(function() {
+  $( ".menu" ).slideToggle( "slow", function() {
+  $( ".canva_expander" ).hide();
+  $( ".cross" ).show();
+  });
+  });
+  
+  $( ".cross" ).click(function() {
+  $( ".menu" ).slideToggle( "slow", function() {
+  $( ".cross" ).hide();
+  $( ".canva_expander" ).show();
+  });
+  });
+  
+  });
+
+ // Close out sub menu
+ $('.sub__close').click(function(e) {
+  e.preventDefault();
+  
+  $(this).parent().parent().removeClass('is-active');
+});
+
+// Trigger sub menu
+$('.menu ul .nav__submenu').click(function(e) {
+  e.preventDefault();
+  
+  $(this).siblings().addClass('is-active');
+});
+
+
 // sticky header
 
 $(window).on("scroll touchmove", function() {
@@ -50,3 +88,4 @@ $(window).on("scroll touchmove", function() {
   }
   
 });
+
